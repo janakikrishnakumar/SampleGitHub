f71374ffe1c95ec93fb898e4c0aecbc54ace0951
janakikrishnakumar <januraja3@gmail.com>
2024-08-08 15:45:51 +0530

    Succesfully completed

---
+++ src/main/java/gitHub/FirstSample.java
@@ -0,0 +1,11 @@
+package gitHub;
+
+public class FirstSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to GitHub");
+
+	}
+
+}
---
+++ src/main/java/gitHub/SecondSample.java
@@ -0,0 +1,10 @@
+package gitHub;
+
+public class SecondSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("GitHub Launched Completely");
+	}
+
+}
